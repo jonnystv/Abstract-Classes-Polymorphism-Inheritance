2c324df9c0cb7685aee9442d5db9269dc443a671
jonnystv <jonathan@darefoundation.com>
2021-02-16 00:06:53 +0000

    Dodgems tests completed

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(10, 1.50, 50.00);
+        visitor2 = new Visitor(13, 1.40, 50.00);
+
     }
 
 
@@ -28,5 +34,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(4.50, dodgems.defaultPrice(), 0.0 );
+    }
+
+    @Test
+    public void underTwelveHalfPrice() {
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.0);
+    }
+
+    @Test
+    public void twelvOrOverDefaultPrice() {
+        assertEquals(4.5, dodgems.priceFor(visitor2), 0.0);
     }
 }
