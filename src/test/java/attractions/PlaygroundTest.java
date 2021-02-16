08d9998e1092d72dda7492527f8a36db37c2f5c9
jonnystv <jonathan@darefoundation.com>
2021-02-16 00:21:13 +0000

    Playground tests completed

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(13, 1.50, 50.00);
+        visitor2 = new Visitor(18, 2.05, 50.00);
+
     }
 
     @Test
@@ -27,4 +33,15 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void canVisitPlaygroundFifteenOrUnder() {
+        assertEquals(true, playground.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void cannotVisitPlaygroundOverFifteen() {
+        assertEquals(false, playground.isAllowedTo(visitor2));
+    }
+
 }
